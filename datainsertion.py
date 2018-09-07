d4306df27520471f0d5d69e01a013462579ec5e1
Sunnan Islam Rahi <sunnan@arbisoft.com>
2018-09-07 22:24:07 +0500

    Final Submission

--- datainsertion.py
+++ datainsertion.py
@@ -1,6 +1,7 @@
+import json
+
 from triumph import db
 from triumph.models import Category, Product, SubCategory, Sku, ImageUrl
-import json
 
 
 def get_or_create(model, **field):
@@ -26,31 +27,33 @@ for raw_product in data:
     db.session.add(sub_category)
     category.sub_categories.append(sub_category)
 
-    product = Product(id=int(raw_product['retailer_sku']), name=raw_product['name'], brand=raw_product['brand'],
-                      price=raw_product['price'], currency=raw_product['currency'], gender=raw_product['gender'],
-                      description=raw_product['description'], url=raw_product['url'])
+    product = get_or_create(Product, id=int(raw_product['retailer_sku']), name=raw_product['name'],
+                            brand=raw_product['brand'], price=raw_product['price'],
+                            currency=raw_product['currency'], gender=raw_product['gender'],
+                            description=raw_product['description'], url=raw_product['url'])
     db.session.add(product)
     category.products.append(product)
     sub_category.products.append(product)
 
     for image_url in raw_product['image_urls']:
-        url = ImageUrl(url=image_url)
+        url = get_or_create(ImageUrl, url=image_url)
         db.session.add(url)
         product.image_urls.append(url)
 
     skus = raw_product['skus']
 
-    for i in range(1, len(skus)):
-        sku = Sku(color=skus[i]['color'], size=skus[i]['size'],
-                  price=skus[i]['price'], currency=skus[i]['currency'])
+    if len(skus):
+        for i in range(1, len(skus)):
+            sku = get_or_create(Sku, color=skus[i]['color'], size=skus[i]['size'],
+                                price=skus[i]['price'], currency=skus[i]['currency'])
 
-        db.session.add(sku)
+            db.session.add(sku)
 
-        for image_url in skus[i]['image_urls']:
-            url = ImageUrl(url=image_url)
-            db.session.add(url)
-            sku.image_urls.append(url)
+            for image_url in skus[i]['image_urls']:
+                url = get_or_create(ImageUrl, url=image_url)
+                db.session.add(url)
+                sku.image_urls.append(url)
 
-        product.skus.append(sku)
+            product.skus.append(sku)
 
     db.session.commit()
